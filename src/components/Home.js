de1f2b02b91818af5954416395ab3a2eb24d1152
RAHULBAWA <rahulbawa514@gmail.com>
2022-11-12 18:22:03 +0530

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,66 @@
 import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
+import New from './NewDisney';
+import Originals from './Originals';
+import Recommendation from './Recommendation';
+import Trending from './Trending';
 import Viewers from './Viewers';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import db from "../firebase"
+import db from '../firebase';
+import { doc, onSnapshot, collection, query, where, QuerySnapshot } from "firebase/firestore";
 
+import {setMovies} from '../features/movie/movieSlice'
+import {selectUserName} from '../features/user/userSlice'
 
 const Home = (props) => {
+  const dispatch=useDispatch();
+  const userName=useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(()=>{
+    const q =query(collection(db,"movies"))
+    const unsub = onSnapshot(q, (QuerySnapshot)=>{
+      QuerySnapshot.docs.map((doc)=>{
+        // console.log(recommends)
+        switch(doc.data().type){
+          case'recommend':
+          recommends=[...recommends,{id:doc.id, ...doc.data()}]
+          break;
+          case'new':
+          newDisneys=[...newDisneys,{id:doc.id, ...doc.data()}]
+          break;
+          case'original':
+          originals=[...originals,{id:doc.id, ...doc.data()}]
+          break;
+          case'trending':
+          trending=[...trending,{id:doc.id, ...doc.data()}]
+          break;
+        }
+      })
+   
+    // db.collection('movies').onSnapshot((snapshot)=>)
+    dispatch(setMovies({
+      recommend:recommends,
+      newDisney:newDisneys,
+      original:originals,
+      trending:trending
+    }))
+  })
+  },[userName])
+
   return (
     <Container>
         <ImgSlider/>
         <Viewers/>
+        <Recommendation/>
+        <New/>
+        <Originals/>
+        <Trending/>
     </Container>
   )
 }
